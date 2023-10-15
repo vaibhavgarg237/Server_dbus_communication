65c4974b7771d969c41f2abd6289604c972cdd56
Vaibhav Garg <vaibhav.garg237@gmail.com>
2023-10-15 20:09:18 +0530

    working -> minimised

--- main.cpp
+++ main.cpp
@@ -1,37 +1,23 @@
 #include <dbus/dbus.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include<iostream>
+using namespace std;
+
 static void check_and_abort(DBusError *error);
 static DBusHandlerResult handle_messages(DBusConnection *connection, DBusMessage *message, void *user_data);
  
 int main() {
     DBusConnection *connection = NULL;
     DBusError error;
-    DBusMessage *msgQuery = NULL;
-    DBusMessage *msgReply = NULL;
-    const char *interfaceName = NULL;
-    const char *versionValue = NULL;
  
     dbus_error_init(&error);
     connection = dbus_bus_get(DBUS_BUS_SESSION, &error);
     check_and_abort(&error);
  
-    interfaceName = "org.KubOS.PowerManager";
 
     dbus_connection_add_filter(connection, handle_messages, NULL, NULL);
-
     dbus_bus_add_match(connection, "type='signal',interface='org.KubOS.PowerManager.PowerStatus'", NULL);
-    dbus_connection_flush(connection);
-
-    DBusObjectPathVTable vtable;
-    vtable.message_function = handle_messages;
-    vtable.unregister_function = NULL;
-    dbus_connection_try_register_object_path(connection,
-                        "/org/KubOS/client",
-                        &vtable,
-                        NULL,
-                        &error);
 
     while (1)
     {
